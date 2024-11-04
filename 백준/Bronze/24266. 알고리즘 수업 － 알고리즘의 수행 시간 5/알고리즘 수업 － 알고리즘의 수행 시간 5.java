dcd45b309deda28e55002cbb8560d53691d56d61
kimjunseo <58328544+wnstj1548@users.noreply.github.com>
2024-11-04 11:54:02 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 116 ms, Memory: 15740 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+
+        bw.write(n * n * n  + "\n3\n");
+        bw.flush();
+        bw.close();
+
+    }
+}
