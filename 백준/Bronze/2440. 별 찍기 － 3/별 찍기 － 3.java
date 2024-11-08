4f9e835407e938185bd73efa5cba3b109393357e
kimjunseo <58328544+wnstj1548@users.noreply.github.com>
2024-11-08 10:33:37 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i<n; i++) {
+            for(int j = n-i; j > 0; j--) {
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
