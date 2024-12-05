c6a18f1fa2b83b7b878ab5bb364abe8864bb3490
kimjunseo <58328544+wnstj1548@users.noreply.github.com>
2024-12-05 09:29:34 +0900

    [Silver II] Title: N과 M (9), Time: 280 ms, Memory: 30956 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static BufferedWriter bw;
+
+    public static void dfs(int depth, int[] temp, boolean[] visited) throws IOException {
+
+        if(depth == m) {
+            for(int i : temp) {
+                bw.write(i + " ");
+            }
+            bw.newLine();
+            return;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(!visited[i] && i == 0) {
+                visited[i] = true;
+                temp[depth] = arr[i];
+                dfs(depth + 1 ,temp, visited);
+                visited[i] = false;
+            } else if(!visited[i] && (arr[i-1] != arr[i] || visited[i-1])) {
+                visited[i] = true;
+                temp[depth] = arr[i];
+                dfs(depth + 1 ,temp, visited);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        int[] temp = new int[m];
+        boolean[] visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0, temp, visited);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
