a4d37f57fde9d9519ec322a7ad091cec0477b96e
kimjunseo <58328544+wnstj1548@users.noreply.github.com>
2024-12-06 14:33:07 +0900

    [Gold V] Title: 강의실 배정, Time: 632 ms, Memory: 69604 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] time = new int[n][2];
+        Queue<Integer> queue = new PriorityQueue<>();
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, (o1, o2) -> {
+
+            if(o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+
+            return o1[0] - o2[0];
+        });
+
+        for(int i = 0; i < n; i++) {
+
+            if(!queue.isEmpty() && queue.peek() <= time[i][0]) {
+                queue.poll();
+            }
+
+            queue.add(time[i][1]);
+        }
+
+        bw.write(queue.size() + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
