5d1142aa7293a7598da6099f20978e88b5ce8255
kimjunseo <58328544+wnstj1548@users.noreply.github.com>
2026-02-17 01:51:06 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -3,46 +3,51 @@ import java.util.*;
 
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static boolean combi(int depth, int start, int[] arr, int[] result) throws IOException {
+
+        if(depth == 7) {
+
+            int sum = 0;
+
+            for(int n : result) {
+                sum += n;
+            }
+
+            if(sum == 100) {
+                Arrays.sort(result);
+                for(int n : result) {
+                    bw.write(n + "");
+                    bw.newLine();
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        for(int i = start; i < arr.length; i++) {
+            result[depth] = arr[i];
+            if(combi(depth + 1, i + 1, arr, result)) return true;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int sum = 0;
+        int[] arr = new int[9];
 
-        List<Integer> intList = new ArrayList<>();
-
-        for(int i = 0; i<9; i++) {
-            int n = Integer.parseInt(br.readLine());
-
-            sum += n;
-            intList.add(n);
+        for(int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        sum -= 100;
-        boolean found = false;
-
-        for(int i = 0; i<intList.size(); i++) {
-            for(int j = i+1; j<intList.size(); j++) {
-                if(intList.get(i) + intList.get(j) == sum) {
-                    intList.remove(j);
-                    intList.remove(i);
-                    found = true;
-                    break;
-                }
-            }
-            if(found) break;
-        }
-
-        Collections.sort(intList);
-
-        if(intList.size() == 7) {
-            for(int i : intList) {
-                bw.write(i + "\n");
-            }
-        }
+        combi(0, 0, arr, new int[7]);
 
         bw.flush();
-        bw.close();
         br.close();
+        bw.close();
     }
 }
